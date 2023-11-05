bf13d81293707befb96666e6da890f0775f8c90a
simoxii48 <mohahejjam@gmail.com>
2023-11-05 22:06:25 +0300

    index route added

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require('express')
 const app = express()
+const router = express.Router()
+
+app.use(express.json())
+
+router.get("/",(req,res) => {
+    res.json("Hello World!")
+})
 
 
-
+app.listen(3000,()=> {
+    console.log("app running on port 3000");
+})
